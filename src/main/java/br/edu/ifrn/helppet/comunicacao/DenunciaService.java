d0a3a1b388a23a8146c41255f21164e906c9d8fb
AnaGoncalo <https://github.com/AnaGoncalo>
2017-06-25 15:48:42 -0300

    Testando a camada de comunicação: metodo POST com spring

--- src/main/java/br/edu/ifrn/helppet/comunicacao/DenunciaService.java
+++ src/main/java/br/edu/ifrn/helppet/comunicacao/DenunciaService.java
@@ -2,7 +2,10 @@ package br.edu.ifrn.helppet.comunicacao;
 
 import br.edu.ifrn.helppet.dominio.Denuncia;
 import br.edu.ifrn.helppet.validacao.DenunciaVL;
+import com.google.gson.Gson;
 import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @author anne
  */
 @RestController
+@RequestMapping("/rest/denuncia")
 public class DenunciaService {
-    
-    @RequestMapping("/rest/denuncia")
-    public List<Denuncia> Listar(){
-	DenunciaVL servico = new DenunciaVL();
-	return servico.ListarDenuncia();
+
+    @GET
+    public List<Denuncia> Listar() {
+        DenunciaVL servico = new DenunciaVL();
+        return servico.ListarDenuncia();
+    }
+
+    @POST
+    public String cadastrarDenuncia(String json) {
+        Gson gson = new Gson();
+        Denuncia a = gson.fromJson(json, Denuncia.class);
+        DenunciaVL servico = new DenunciaVL();
+        servico.CadastrarDenuncia(a);
+        return "Ok";
     }
 }
