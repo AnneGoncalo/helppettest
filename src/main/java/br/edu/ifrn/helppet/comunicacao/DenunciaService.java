7b01ee53b2266f24b696c277af5d6718ab017b84
AnaGoncalo <https://github.com/AnaGoncalo>
2017-06-25 19:46:03 -0300

    Testando a camada de comunicação: colocando o rest com spring de volta

--- src/main/java/br/edu/ifrn/helppet/comunicacao/DenunciaService.java
+++ src/main/java/br/edu/ifrn/helppet/comunicacao/DenunciaService.java
@@ -1,26 +1,35 @@
 package br.edu.ifrn.helppet.comunicacao;
 
+import br.edu.ifrn.helppet.dominio.Denuncia;
+import br.edu.ifrn.helppet.validacao.DenunciaVL;
+import java.util.List;
+import org.json.simple.parser.JSONParser;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author anne
  */
-//@RestController
-//@RequestMapping("/rest/denuncia")
+@RestController
+@RequestMapping("/rest/denuncia")
 public class DenunciaService {
 
-//    @RequestMapping(method = RequestMethod.GET)
-//    public List<Denuncia> Listar() {
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Denuncia> Listar() {
+        DenunciaVL servico = new DenunciaVL();
+        return servico.ListarDenuncia();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String cadastrarDenuncia(@RequestBody String json) {
+//        Gson gson = new Gson();
+//        Denuncia a = gson.fromJson(json, Denuncia.class);
 //        DenunciaVL servico = new DenunciaVL();
-//        return servico.ListarDenuncia();
-//    }
-//
-//    @RequestMapping(method = RequestMethod.POST)
-//    public String cadastrarDenuncia(@RequestBody String json) {
-////        Gson gson = new Gson();
-////        Denuncia a = gson.fromJson(json, Denuncia.class);
-////        DenunciaVL servico = new DenunciaVL();
-////        servico.CadastrarDenuncia(a);
-//          JSONParser parser = new JSONParser();
-//        return "Ok";
-//    }
+//        servico.CadastrarDenuncia(a);
+          JSONParser parser = new JSONParser();
+        return "Ok";
+    }
 }
