c68250d02a915e6ecc3138d39cbbaae006c2d251
AnaGoncalo <https://github.com/AnaGoncalo>
2017-06-25 17:02:36 -0300

    Testando a camada de comunicação: agora sem spring

--- src/main/java/br/edu/ifrn/helppet/comunicacao/DenunciaService.java
+++ src/main/java/br/edu/ifrn/helppet/comunicacao/DenunciaService.java
@@ -16,23 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author anne
  */
-@RestController
-@RequestMapping("/rest/denuncia")
+//@RestController
+//@RequestMapping("/rest/denuncia")
 public class DenunciaService {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Denuncia> Listar() {
-        DenunciaVL servico = new DenunciaVL();
-        return servico.ListarDenuncia();
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public String cadastrarDenuncia(@RequestBody String json) {
-//        Gson gson = new Gson();
-//        Denuncia a = gson.fromJson(json, Denuncia.class);
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<Denuncia> Listar() {
 //        DenunciaVL servico = new DenunciaVL();
-//        servico.CadastrarDenuncia(a);
-          JSONParser parser = new JSONParser();
-        return "Ok";
-    }
+//        return servico.ListarDenuncia();
+//    }
+//
+//    @RequestMapping(method = RequestMethod.POST)
+//    public String cadastrarDenuncia(@RequestBody String json) {
+////        Gson gson = new Gson();
+////        Denuncia a = gson.fromJson(json, Denuncia.class);
+////        DenunciaVL servico = new DenunciaVL();
+////        servico.CadastrarDenuncia(a);
+//          JSONParser parser = new JSONParser();
+//        return "Ok";
+//    }
 }
