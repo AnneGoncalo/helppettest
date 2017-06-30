7e2298dd663fc6054771bfe6b87eddba64b549c4
camilajordana <c_jordana@hotmail.com>
2017-06-29 22:37:00 -0300

    Adicionando validação do CPF

--- src/main/java/br/edu/ifrn/helppet/validacao/UsuarioVL.java
+++ src/main/java/br/edu/ifrn/helppet/validacao/UsuarioVL.java
@@ -15,8 +15,10 @@
  */
 package br.edu.ifrn.helppet.validacao;
 
+import br.edu.ifrn.helppet.dominio.PessoaFisica;
 import br.edu.ifrn.helppet.dominio.Usuario;
 import br.edu.ifrn.helppet.persistencia.PersistenciaGamb;
+import java.util.InputMismatchException;
 
 /**
  *
@@ -257,6 +259,7 @@ public class UsuarioVL {
     }
 
     // Verifica se a conta que será excluída pertence ao usuário que deseja excluí-la
+    // Dúvida
     public String excluirConta(Usuario u1, Usuario u2) {
         if (u1.equals(u2)) {
             return "Conta excluída";
@@ -264,5 +267,144 @@ public class UsuarioVL {
             return "Conta não encontrada";
         }
     }
+    
+    public String validarCPF(PessoaFisica pf) {
+        String cpfFormatado = null;
+        if (pf.getCpf() != null && !pf.getCpf().equals("")) {
+            if (pf.getCpf().length() == 11) {
+                if (pf.getCpf().equals("00000000000") || pf.getCpf().equals("11111111111")
+                        || pf.getCpf().equals("22222222222") || pf.getCpf().equals("33333333333")
+                        || pf.getCpf().equals("44444444444") || pf.getCpf().equals("55555555555")
+                        || pf.getCpf().equals("66666666666") || pf.getCpf().equals("77777777777")
+                        || pf.getCpf().equals("88888888888") || pf.getCpf().equals("99999999999")) {
+                    return "Informe um CPF válido";
+                } else {
+                    char dig10, dig11;
+                    int sm, i, r, num, peso;
+
+                    try { // protege o codigo para eventuais erros de conversao de tipo (int)
+                        // Cálculo do 1º dígito verificador
+                        sm = 0;
+                        peso = 10;
+                        for (i = 0; i < 9; i++) {
+                            // converte o i-esimo caractere do CPF em um numero:
+                            // por exemplo, transforma o caractere '0' no inteiro 0         
+                            // (48 eh a posicao de '0' na tabela ASCII)         
+                            num = (int) (pf.getCpf().charAt(i) - 48);
+                            sm = sm + (num * peso);
+                            peso = peso - 1;
+                        }
+
+                        r = 11 - (sm % 11);
+                        if ((r == 10) || (r == 11)) {
+                            dig10 = '0';
+                        } else {
+                            dig10 = (char) (r + 48); // converte no respectivo caractere numerico
+                        }
+                        // Cálculo do 2º dígito verificador
+                        sm = 0;
+                        peso = 11;
+                        for (i = 0; i < 10; i++) {
+                            num = (int) (pf.getCpf().charAt(i) - 48);
+                            sm = sm + (num * peso);
+                            peso = peso - 1;
+                        }
+
+                        r = 11 - (sm % 11);
+                        if ((r == 10) || (r == 11)) {
+                            dig11 = '0';
+                        } else {
+                            dig11 = (char) (r + 48);
+                        }
+
+                        // Verifica se os digitos calculados conferem com os digitos informados.
+                        if ((dig10 == pf.getCpf().charAt(9)) && (dig11 == pf.getCpf().charAt(10))) {
+                            return "OK";
+                        } else {
+                            return "Informe um CPF válido";
+                        }
+                    } catch (InputMismatchException erro) {
+                        return "Informe um CPF válido";
+                    }
+                }
+            } else if (pf.getCpf().length() == 14) {
+
+                char[] c = pf.getCpf().toCharArray();
+                String cpf_ = "";
+                int cont = 0;
+                if (c[3] == '.' && c[7] == '.' && c[11] == '-') {
+
+                    for (int i = 0; i < c.length; i++) {
+                        if (Character.isDigit(c[i])) {
+                            cpf_ = cpf_ + c[i];
+                        }
+                    }
+
+                    char dig10, dig11;
+                    int sm, i, r, num, peso;
+
+                    try { // protege o codigo para eventuais erros de conversao de tipo (int)
+                        // Cálculo do 1º dígito verificador
+                        sm = 0;
+                        peso = 10;
+                        for (i = 0; i < 9; i++) {
+                            // converte o i-esimo caractere do CPF em um numero:
+                            // por exemplo, transforma o caractere '0' no inteiro 0         
+                            // (48 eh a posicao de '0' na tabela ASCII)         
+                            num = (int) (cpf_.charAt(i) - 48);
+                            sm = sm + (num * peso);
+                            peso = peso - 1;
+                        }
+
+                        r = 11 - (sm % 11);
+                        if ((r == 10) || (r == 11)) {
+                            dig10 = '0';
+                        } else {
+                            dig10 = (char) (r + 48); // converte no respectivo caractere numerico
+                        }
+                        // Cálculo do 2º dígito verificador
+                        sm = 0;
+                        peso = 11;
+                        for (i = 0; i < 10; i++) {
+                            num = (int) (cpf_.charAt(i) - 48);
+                            sm = sm + (num * peso);
+                            peso = peso - 1;
+                        }
+
+                        r = 11 - (sm % 11);
+                        if ((r == 10) || (r == 11)) {
+                            dig11 = '0';
+                        } else {
+                            dig11 = (char) (r + 48);
+                        }
+
+                        // Verifica se os digitos calculados conferem com os digitos informados.
+                        if ((dig10 == cpf_.charAt(9)) && (dig11 == cpf_.charAt(10))) {
+                            return "OK";
+                        } else {
+                            return "Informe um CPF válido";
+                        }
+                    } catch (InputMismatchException erro) {
+                        return "Informe um CPF válido";
+                    }
+
+                } else {
+                    return "Informe um CPF válido";
+                }
+
+            } else {
+                return "Informe um CPF válido";
+            }
+        } else {
+            return "OK";
+        }
+    }
+
+    public static String imprimeCPF(String CPF) {
+        return (CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "."
+                + CPF.substring(6, 9) + "-" + CPF.substring(9, 11));
+    }
+
+    
 
 }
