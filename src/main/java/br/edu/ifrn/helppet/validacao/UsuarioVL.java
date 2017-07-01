d5a2cbb2c07dcfa96a0aa8f7dfc6f5f2af89e71c
camilajordana <c_jordana@hotmail.com>
2017-07-01 14:02:45 -0300

    Validação evento (falta um campo)

--- src/main/java/br/edu/ifrn/helppet/validacao/UsuarioVL.java
+++ src/main/java/br/edu/ifrn/helppet/validacao/UsuarioVL.java
@@ -401,11 +401,12 @@ public class UsuarioVL {
         }
     }
 
-    public static String imprimeCPF(String CPF) {
+    private static String imprimeCPF(String CPF) {
         return (CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "."
                 + CPF.substring(6, 9) + "-" + CPF.substring(9, 11));
     }
 
+    
     
 
 }
